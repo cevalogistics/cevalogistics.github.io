5eac6f08cc0a0d8e2864c8e847983e8d4300584b
rsliotta <robert.liotta@cevalogistics.com>
2019-12-03 14:38:49 +0000

    Refresh Repository

--- init.sh
+++ init.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
 
-git pull  
-rm -rf ./helm-chart-sources 2>/dev/null
-mkdir ./helm-chart-sources && cd ./helm-chart-sources/
+  
+cd helm-repository  
 
-git clone https://github.com/cevalogistics/etcd-make-mirror.git
-git clone https://github.com/cevalogistics/multi-cluster-dns.git
+helm lint ../charts/stable/*
+helm package ../charts/stable/*
 
-cd ..
+helm repo index --url https://cevalogistics.github.io/ . 2>/dev/null
 
-helm lint helm-chart-sources/*
+mkdir work  && cd work
+ 
+set -x 
 
-helm package helm-chart-sources/*
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_USERNAME"
 
-helm repo index --url https://cevalogistics.github.io/ .
+git clone https://github.com/cevalogistics/cevalogistics.github.io
+
+cd cevalogistics.github.io
+
+cp ../../* . 2>/dev/null
 
 git add .
-git commit -m 'Refresh'
-git push
+git commit -m 'Refresh Repository'
+
+git push --repo https://$GIT_USERNAME:$GIT_PASSWORD@github.com/cevalogistics/cevalogistics.github.io
